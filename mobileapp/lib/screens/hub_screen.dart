bc11b9739d08ee3ea2aa2b9824b555030a58e4c9
alexp <alexander-kn@bk.ru>
2021-01-28 10:46:56 +0300

    added widgets to the screen

--- mobileapp/lib/screens/hub_screen.dart
+++ mobileapp/lib/screens/hub_screen.dart
@@ -29,74 +29,128 @@ class HubScreen extends StatelessWidget {
       trailing: Clickable(
           onPressed: () => showConfirm(title: developmentText),
           child: Icon(Icons.notifications)),
-      child: Column(children: [
-        VerticalSpace(),
-        GetBuilder<HubController>(
-            init: HubController(),
-            builder: (controller) =>
-                Stack(alignment: Alignment.center, children: [
-                  RotationTransition(
-                      turns: Tween(begin: .0, end: 1.0)
-                          .animate(controller.animationController),
-                      child: Stack(alignment: Alignment.center, children: [
-                        CustomPaint(
-                            size: M.Size(outerRadius, outerRadius),
-                            painter: SectorProgressPainter(
-                                color: Colors.nutrition,
-                                title: 'ПИТАНИЕ',
-                                value: .63,
-                                endAngle: angle,
-                                startAngle: startAngle)),
-                        CustomPaint(
-                            size: M.Size(outerRadius, outerRadius),
-                            painter: SectorProgressPainter(
-                                color: Colors.exercise,
-                                title: 'ДВИЖЕНИЕ',
-                                value: .38,
-                                endAngle: angle,
-                                startAngle: startAngle + angle)),
-                        CustomPaint(
-                            size: M.Size(outerRadius, outerRadius),
-                            painter: SectorProgressPainter(
-                                color: Colors.schedule,
-                                title: 'РЕЖИМ',
-                                value: .81,
-                                endAngle: angle,
-                                startAngle: startAngle + angle * 2))
-                      ])),
-                  CircularProgress(
-                      size: radius,
-                      child: Text(
-                        '64%',
-                        style: TextStyle.primary
-                            .copyWith(fontSize: Size.fontPercent),
-                      ))
-                ])),
-        Row(children: [
-          HorizontalSpace(),
-          Expanded(
-              child: Button(
-                  padding: EdgeInsets.symmetric(vertical: Padding.button.top),
-                  title: 'ПИТАНИЕ',
-                  borderColor: Colors.nutrition,
-                  titleStyle: M.TextStyle(fontSize: Size.width(12)))),
-          HorizontalSmallSpace(),
-          Expanded(
-              child: Button(
-                  padding: EdgeInsets.symmetric(vertical: Padding.button.top),
-                  title: 'ДВИЖЕНИЕ',
-                  borderColor: Colors.exercise,
-                  titleStyle: M.TextStyle(fontSize: Size.width(12)))),
-          HorizontalSmallSpace(),
-          Expanded(
-              child: Button(
-                  padding: EdgeInsets.symmetric(vertical: Padding.button.top),
-                  title: 'РЕЖИМ',
-                  borderColor: Colors.schedule,
-                  titleStyle: M.TextStyle(fontSize: Size.width(12)))),
-          HorizontalSpace()
-        ])
-      ]));
+      child: ListView(
+        children: [Column(children: [
+          VerticalSpace(),
+          GetBuilder<HubController>(
+              init: HubController(),
+              builder: (controller) =>
+                  Stack(alignment: Alignment.center, children: [
+                    RotationTransition(
+                        turns: Tween(begin: .0, end: 1.0)
+                            .animate(controller.animationController),
+                        child: Stack(alignment: Alignment.center, children: [
+                          CustomPaint(
+                              size: M.Size(outerRadius, outerRadius),
+                              painter: SectorProgressPainter(
+                                  color: Colors.nutrition,
+                                  title: nutritionTitle,
+                                  value: .63,
+                                  endAngle: angle,
+                                  startAngle: -pi / 2)),
+                          CustomPaint(
+                              size: M.Size(outerRadius, outerRadius),
+                              painter: SectorProgressPainter(
+                                  color: Colors.exercise,
+                                  title: exerciseTitle,
+                                  value: .38,
+                                  endAngle: angle,
+                                  startAngle: startAngle + angle)),
+                          CustomPaint(
+                              size: M.Size(outerRadius, outerRadius),
+                              painter: SectorProgressPainter(
+                                  color: Colors.schedule,
+                                  title: scheduleTitle,
+                                  value: .81,
+                                  endAngle: angle,
+                                  startAngle: startAngle + angle * 2))
+                        ])),
+                    CircularProgress(
+                        size: radius,
+                        child: Text(
+                          '64%',
+                          style: TextStyle.primary
+                              .copyWith(fontSize: Size.fontPercent),
+                        ))
+                  ]))
+        ]),
+          VerticalBigSpace(),
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  child:
+                  Row(children: [
+                    HorizontalSpace(),
+                    Expanded(child: Clickable(
+                      child: StrokedButtonWidget(title: scheduleTitle,
+                        borderColor: Colors.schedule,),
+                    )),
+                    HorizontalSmallSpace(),
+                    Expanded(child: Clickable(
+                      child: StrokedButtonWidget(title: nutritionTitle,
+                        borderColor: Colors.nutrition,),
+                    )),
+                    HorizontalSmallSpace(),
+                    Expanded(child: Clickable(
+                      child: StrokedButtonWidget(title: exerciseTitle,
+                        borderColor: Colors.exercise,),
+                    )),
+                    HorizontalSpace(),
+                  ],),),
+                Container(color: Colors.background,
+                  padding: EdgeInsets.symmetric(horizontal: Size.horizontal),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      VerticalBigSpace(),
+                      RowTitleAndDescriptionWidget( title: 'Цель:', description: 'Похудение',),
+                      VerticalMediumSpace(),
+                      RowTitleAndDescriptionWidget( title: 'Макроцикл:', description: 'Оздоровительный',),
+                      VerticalMediumSpace(),
+                      Column(
+                        children: [
+                          RowTitleAndDescriptionWidget( title: 'Микроцикл:', description: 'Первый',),
+                          RowTitleAndDescriptionWidget( title: '(2 м /16тр)', description: '(подготовительный)',),
+                        ],
+                      ),
+                      VerticalMediumSpace(),
+                      RowTitleAndDescriptionWidget( title: 'Неделя:', description: '2/8',),
+                      VerticalMediumSpace(),
+                      RowTitleAndDescriptionWidget( title: 'Тренировки:', description: '3/16',),
+                      VerticalBigSpace(),
+                    ],
+                  ),
+                ),
+                Container(color: Colors.background,
+                  padding: EdgeInsets.symmetric(horizontal: Size.horizontal),
+                  child: Column(
+                    children: [
+                      CardWidget(title: 'ПРОДУКТЫ ЧЕМПИОНЫ Ч.3',),
+                      VerticalSpace(),
+                      Row(
+                        children: [
+                          CardWidget(
+                            width: Size.avatar,
+                            title: 'КРУНИЕ НАЧАЛО КОНЦА',
+                          ),
+                        HorizontalSpace(),
+                        CardWidget(
+                          width: Size.avatar,
+                          title: 'ЧЕМ СИЛЕН СЕЛЕН?',
+                        ),
+                        ],),
+                      VerticalSpace(),
+                      CardWidget(title: 'НАШ ОРГАНИЗМ И ВЛИЯНИЕ НА НЕГО НАШЕГО РАЦИОНА',),
+                      VerticalBigSpace(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ],)
+  );
 }
 
 class HubController extends Controller with SingleGetTickerProviderMixin {
