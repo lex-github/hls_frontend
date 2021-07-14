6954a01e436a5f3de42553224ed3cfdd9a6bd7fb
Lex <lex@MacBook-Air-2.local>
2021-07-14 13:38:06 +0300

    notifications integrated on ios

---
+++ mobileapp/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Lex on 14.07.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
